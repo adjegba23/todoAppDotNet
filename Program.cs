217fee8863d5ab57aeec2257f5d42f70ee654548
Kaleb Smith <kaleb.smith2@gmail.com>
2022-09-26 16:53:08 -0400

    Project Init and Git Ignore

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ToDoApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's just a test!");
+        }
+    }
+}
